ee6307959d621bf2c52acb60f2e8b283c6a2c3b2
Coder4916 Bailey <c.bailey84@hotmail.com>
2023-11-14 18:34:23 +0000

    added functions based on what I think the game will need to work

---
+++ script.js
@@ -0,0 +1,31 @@
+function startGame(){
+
+}
+
+function checkCards(){
+
+}
+
+function checkCorrectCards(){
+
+}
+
+function logScore(){
+
+}
+
+function logNoMatch(){
+
+}
+
+function displayEasyGrid(){
+
+}
+
+function displayMediumGrid(){
+
+}
+
+function displayHardGrid(){
+    
+}
