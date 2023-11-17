334d852bbd2966987fa571fe4f348c311357732b
Coder4916 <>
2023-11-17 21:23:55 +0000

    adjusted game grid and cards to make more responsive and fit the screen better

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let cards = document.getElementsByClassName('card');
-let hasFlippedCard = false;
+let cardFlipped = false;
 let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip')
+    this.classList.add('flip')
+
+    if(!cardFlipped) {
+        cardFlipped = true;
+        firstCard = this;
+    } else{
+        cardFlipped = false;
+        secondCard = this;
+
+        console.log(firstCard, secondCard)
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
