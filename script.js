abc76e19c3d3768f88127161a32b24435de2a98b
Coder4916 <>
2023-11-19 19:23:19 +0000

    refactoring on script.js file completed

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-let cards = document.getElementsByClassName('card'); //Used to create an event listener on all cards//
+const cards = document.querySelectorAll('.card'); //Used to create an event listener on all cards//
+
 let cardFlipped = false; //Initially no cards have been flipped so cardFlipped is set to false//
 let firstCard, secondCard; //A variable is assigned firstCard and secondCard to acknowledge when these are clicked by the user//
 
 //A function to monitor the first and second cards clicked on, with the 'this' event used to monitor these//
 
 function flipCard() {
-    this.classList.add('flip')
-
-    if(!cardFlipped) {
+    this.classList.add('flip');
+    if (!cardFlipped) {
         cardFlipped = true;
         firstCard = this;
-    } else{
-        cardFlipped = false;
-        secondCard = this;
+
+        return;
+    }
+
+    cardFlipped = false;
+    secondCard = this;
+    checkCardsMatch();
+    
+}
+
+function checkCardsMatch() {
+    if (firstCard.getAttribute("framework") ===
+        secondCard.getAttribute("framework")) {
+        collectCards();
+    } else {
+        returnCards();
     }
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard))
+function collectCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function returnCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
